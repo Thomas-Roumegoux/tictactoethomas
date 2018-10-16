26a68a38d535f7f529a2548f774100b0bb32adf4
tomroumeg <tomroumeg@gmail.com>
2018-10-16 23:17:34 +0200

    tictactoe

---
+++ README.md
@@ -0,0 +1,22 @@
+Tic Tac Toe Amazing game!
+
+Après la journée d'hier tu as besoin de détente ?
+
+Ça tombe bien j'ai un jeu pour toi le Tic Tac Toe
+
+Un jeu d'adresse et de sagesse avec 2 joueurs mais 1 seul vainqueur
+
+Règle du jeu: 
+
+-Quand on te demande de saisir un chiffre tu saisis un chiffre entre 1 et 9 (ni plus, ni moins) 
+
+-L’objectif est de réussir à aligner trois ronds ou trois croix dans une grille de 3 sur 3.
+
+-Les joueurs choisissent un des éléments, et jouent en alternance.
+
+-La partie s’arrête lorsque l’un des joueurs a réussi à aligner trois éléments, ou lorsque les 9 cases ont été remplies.
+
+It's an exercise from the TheHackingProject bootcamp.
+> www.thehackingproject.org
+
+Mais techniquement comment ça fonctionne ? Et bien click, et le savoir tu auras https://files.slack.com/files-pri/T3W8B88JW-FDGBC3WF8/tictactoe_pseudouml.jpg.
---
+++ ttt.rb
@@ -0,0 +1,114 @@
+class BoardCase
+  attr_accessor :statue, :content, :position
+
+	def initialize
+		@statue = true
+		@content = " "
+		@position = 1
+	end
+
+end
+class Board
+	attr_accessor :board
+	include Enumerable
+
+	def initialize
+		@board = []
+		@board[0] = @content
+		@board[1] = @content
+		@board[2] = @content
+		@board[3] = @content
+		@board[4] = @content
+		@board[5] = @content
+		@board[6] = @content
+		@board[7] = @content
+		@board[8] = @content
+	end
+
+	def display
+		puts " #{@board[0]}  | #{@board[1]} | #{@board[2]} "
+		puts "----------"
+		puts " #{@board[3]}  | #{@board[4]} | #{@board[5]} "
+		puts "----------"
+		puts " #{@board[6]}  | #{@board[7]} | #{@board[8]} "
+	end
+
+	def play(ma_piece)
+		@board[@position.to_i - 1] = ma_piece.to_s
+		return @board
+	end
+
+	def victory?
+		@win_status = 0
+		if (@board[0] == @board[1] && @board[0] == @board[2]) || (@board[3] == @board[4]&& @board[3] == @board[5]) || (@board[7] == @board[8] && @board[7] == @board[9]) || (@board[0] == @board[4] && @board[0] == @board[8]) || (@board[6] == @board [4] && @board[6] == @board[2]) || (@board[0] == @board[3] && @board[0] == @board[6]) || (@board[1] == @board[4] && @board[1] == @board[7]) || (@board[2] == @board[5] && @board[2] == @board[8])
+		@win_status = 1
+		return @win_status
+		end
+	end
+end
+class Player
+	attr_accessor :name, :piece
+	attr_writer :win
+
+	def initialize
+		@name = name
+		@piece = piece
+	end
+end
+
+class Game
+	def initialize
+		@b_start = true
+		init_player
+		init_board
+	end
+
+	def init_player
+		puts "Player1, what's your name ?:"
+		@name1 = gets.chomp
+		puts "Player2, what's your name ?:"
+		@name2 = gets.chomp
+		@player1 = Player.new
+		@player1.name = @name1
+		@player1.piece = "X"
+		@player2 = Player.new
+		@player2.name = @name2
+		@player2.piece = "O"
+	end
+
+	def init_board
+	@my_board = Board.new
+	end
+
+
+	def go
+    @i = 1
+		while @i < 9
+			if @i == 1 || @i == 3 || @i == 5 || @i == 7 || @i == 9
+				puts "#{@name1} à toi de jouer ! Choisis une case entre 1 et 9:"
+				turn
+				@my_board.play("X")
+				@i = @i + 1
+			end
+			if @i == 2 || @i == 4 || @i == 6 || @i == 8
+				puts "#{@name2} à toi de jouer ! Choisis une case entre 1 et 9:"
+				turn
+				@my_board.play("O")
+				@i = @i + 1
+			end
+		end
+	end
+	def turn
+	@my_board.display
+	@position = gets.chomp
+	@my_board.victory?
+		if @win_status == 1
+			puts "Bravo tu as gagné !"
+		end
+		if @win_status == 0
+		puts "Match nul comme vous,relancez le programme !"
+		end
+	end
+end
+plateau = 0
+plateau = Game.new.go
